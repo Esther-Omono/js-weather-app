e787ed42b5cea44f5abfdb03d3bd83fd88617f80
Esther-Omono <enitanatakere@gmail.com>
2024-12-19 19:05:33 +0100

    Update the file

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_KEY = '751cf99b011729693acfd27fbd852c04'; // Replace with your actual API key
+const API_KEY = '751cf99b011729693acfd27fbd852c04';
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export async function fetchWeather(location) {
